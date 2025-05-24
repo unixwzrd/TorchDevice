0ac7e1429da5a7ef78e58a4cc8f267b2629e0dfe
M S <unixwzrd.register@mac.com>
2025-05-24 07:51:52 -0500

    didn't merge correctly, updated now.

--- TorchDevice/__init__.py
+++ TorchDevice/__init__.py
@@ -4,15 +4,7 @@ This module patches PyTorch's CUDA functionality to work seamlessly with MPS (an
 upon import. Users should never need to call patch functions directly—patching is automatic.
 """
 
-<<<<<<< HEAD
-<<<<<<< HEAD
-__version__ = '0.0.5'
-=======
-__version__ = '0.1.1'
->>>>>>> 20250427_00-rollback
-=======
 __version__ = '0.2.0'
->>>>>>> rollback-20250506
 
 from .TorchDevice import TorchDevice
 from .modules.TDLogger import auto_log
